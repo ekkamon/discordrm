359a9a934134dca09bdf4658b2332851d0bef34a
Ekkamon Munsri <ekkb.contact@gmail.com>
2023-11-10 23:35:23 +0700

    ♻️ refactor(api/user): clean up unused functions

--- src/api/interval/user/repository.go
+++ src/api/interval/user/repository.go
@@ -28,25 +28,3 @@ func (r repository) Create(user models.User) (models.User, error) {
 
 	return user, nil
 }
-
-func (r repository) GetByUsername(username string) (models.User, error) {
-	user := models.User{}
-
-	res := r.db.PgSql.Where(&models.User{Username: username}).First(&user)
-	if res.Error != nil {
-		return models.User{}, res.Error
-	}
-
-	return user, nil
-}
-
-func (r repository) GetByEmail(email string) (models.User, error) {
-	user := models.User{}
-
-	res := r.db.PgSql.Where(&models.User{Email: email}).First(&user)
-	if res.Error != nil {
-		return models.User{}, res.Error
-	}
-
-	return user, nil
-}
